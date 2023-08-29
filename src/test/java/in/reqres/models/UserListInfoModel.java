55e15f8f97ee32296584894459e9118c8a7f2288
Yulia Buntseva <juliabuntseva@gmail.com>
2023-08-29 19:17:48 +0200

    minor changes to UserListInfoModel.java

--- src/test/java/in/reqres/models/UserListInfoModel.java
+++ src/test/java/in/reqres/models/UserListInfoModel.java
@@ -1,19 +1,30 @@
 package in.reqres.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class UserListInfoModel {
-    int page, per_page, total, total_pages;
+    public int page;
+    @JsonProperty("per_page")
+    public int perPage;
+    public int total;
+    @JsonProperty("total_pages")
+    public int totalPages;
     List<UserData> data;
     Support support;
 
     @Data
     public static class UserData {
-        int id;
-        String email, first_name, last_name, avatar;
+        public int id;
+        public String email;
+        @JsonProperty("first_name")
+        public String firstName;
+        @JsonProperty("last_name")
+        public String lastName;
+        public String avatar;
     }
 
     @Data
